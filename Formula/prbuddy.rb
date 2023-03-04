49e1a98723ffec6552b8a2971bc8680f41b45407
MitchWijt <mitchel@wijt.net>
2023-03-04 09:28:34 +0100

    feat: release new version of prbuddy

--- Formula/prbuddy.rb
+++ Formula/prbuddy.rb
@@ -4,9 +4,9 @@
 class Prbuddy < Formula
   desc "PR creator and publisher CLI written in Rust"
   homepage "https://github.com/MitchWijt/prBuddy"
-  url "https://github.com/MitchWijt/prBuddy/releases/download/v1.0.0/prbuddy-mac.tar.gz"
-  sha256 "184f0c31516f0bd25df6099c613d4462286911f6ab7a7f48d3d036ce290947d2"
-  version "1.0.0"
+  url "https://github.com/MitchWijt/prBuddy/releases/download/v1.1.0/prbuddy-mac.tar.gz"
+  sha256 "e20f8a69883e207303b94c6a62b8e4aabd0a9a6821c848ab4079bc1b12ec3c7a"
+  version "1.1.0"
 
   def install
     bin.install "pr_buddy"
